affad94dad64ee82305e74c8aefe04cb9e8459b1
Tri Nguyen <tringuyen9295@gmail.com>
2023-08-28 19:56:14 +1000

    add basic rendering

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
 app.set("views", "./views");
 
+app.use(express.static("public"));
+
 app.get("/", (_, res) => {
 	res.render("home");
 });
