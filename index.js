98847434a49fba7b0460f341f8e9fceb12d855db
Tri Nguyen <tringuyen9295@gmail.com>
2023-08-28 19:53:03 +1000

    add handlebars for rendering

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import express from "express";
+import { engine } from "express-handlebars";
 
 const app = express();
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "./views");
 
 app.get("/", (_, res) => {
-	res.send("Hello world");
+	res.render("home");
 });
 
 app.listen(3000, () => {
